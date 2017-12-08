b142d6936285834a402e0e2380dbe660888acb24
Hank Stocker <hstocker@gmail.com>
2017-12-07 22:36:19 -0500

    Solve part one

--- day4/high_entropy_passphrase.hs
+++ day4/high_entropy_passphrase.hs
@@ -1,11 +1,15 @@
-import Data.List(nub)
+import Data.List(nub, foldl', mapAccumL)
 import Input(passphrases)
 
-diceData :: String -> [[String]]
-diceData = map words . lines
+dice :: String -> [[String]]
+dice = map words . lines
 
 isUniquePassphrase :: [String] -> Bool
 isUniquePassphrase xs = xs == (nub xs)
+
+countUnique :: String -> Int
+countUnique pass = foldl' (\acc x -> case x of { True -> acc + 1; False -> acc}) 0 counted
+   where counted = (map isUniquePassphrase $ dice pass)
 
 small = "pphsv ojtou brvhsj cer ntfhlra udeh ccgtyzc zoyzmh jum lugbnk \n\
 \ vxjnf fzqitnj uyfck blnl impo kxoow nngd worcm bdesehw \n\
