85b3d5d8e28e77329ebfbf4a3bf843fd473ba118
Hank Stocker <hstocker@gmail.com>
2018-12-04 17:02:38 -0600

    this code is a sin, avert your gaze

--- 2018/day4/Repose.hs
+++ 2018/day4/Repose.hs
@@ -2,9 +2,9 @@ module Challenge where
 
 import           Common
 import           Control.Applicative
-import           Data.Function ((&))
+import           Data.Function
 import           Data.Functor ((<&>))
-import           Data.List (sort, groupBy)
+import           Data.List (sort,sortBy, groupBy)
 import qualified Data.Map.Strict as M
 import           Data.Map.Strict (Map(..))
 import qualified Data.IntMap as IM
@@ -29,12 +29,11 @@ data Shift = Shift
   , start :: LocalTime
   , events :: [LogEntry]
   , sleepPeriods :: [Int]
-  , totalSleep :: SleepMinutes
+  , totalSleep :: Int
   } deriving (Eq, Show)
 
 type Guard = Int
-type SleepMinutes = Int
-data Status = Sleeping Guard | Awake Guard
+type Minute = Int
 data Event = FallAsleep | WakeUp | Begin Guard
   deriving (Eq, Show)
 
@@ -44,66 +43,77 @@ main = do
   logs <- sort <$> parseInput (many (parseLog <* whiteSpace)) [] inputPath
   let logsByShift = groupByShift logs
   let shifts = processShift <$> logsByShift
+  let sleepiestGuard = head $ reverse $ sortBy (compare `on` snd) $ M.toList $ sumSleepByGuard shifts
+  putStrLn $ "Sleepiest guard: " <> (show sleepiestGuard)
+  let mappo = shiftsToMinuteMap shifts
+  let minutes = IM.lookup (fst sleepiestGuard) mappo
+  putStrLn $ "Minutes: " <> (show $ IM.toList minutes)
+  -- sort . fmap (\x -> (length x, x)) . group . sort
 
+  -- [
+  -- Shift {guardId = 1499, 
+  --       start = 1518-03-11 00:04:00, 
+  --       events = [[1518-03-11 00:33:00] FallAsleep,[1518-03-11 00:54:00] WakeUp],
+  --       sleepPeriods = [33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53], 
+  --       totalSleep = 903},
+  -- Shift {guardId = 2657, 
+  --       start = 1518-03-12 00:03:00, 
+  --       events = [[1518-03-12 00:21:00] FallAsleep,[1518-03-12 00:29:00] WakeUp,[1518-03-12 00:35:00] FallAsleep,[1518-03-12 00:47:00] WakeUp], 
+  --       sleepPeriods = [21,22,23,24,25,26,27,28,35,36,37,38,39,40,41,42,43,44,45,46], 
+  --       totalSleep = 682}
+  -- ]
 
-  putStrLn "wut"
+shiftsToMinuteMap :: [Shift] -> IntMap [Minute]
+shiftsToMinuteMap xs = do 
+  let entries =  xs >>= sleepPeriodToEntries
+  IM.fromListWith (<>) entries
+
+sleepPeriodToEntries :: Shift -> [(Guard, [Minute])]
+sleepPeriodToEntries shift = fmap (\x -> (guardId shift, [x])) (sleepPeriods shift)
 
 groupByShift :: [LogEntry] -> [[LogEntry]]
 groupByShift = groupBy sameShift
 
+-- precondition: sorted input
 sameShift :: LogEntry -> LogEntry -> Bool
 sameShift a (LogEntry b _) = case b of
   Begin _ -> False
   _       -> True
 
-summarizeSleepByGuard :: [Shift] -> Map Guard SleepMinutes
-summarizeSleepByGuard xs = xs
-  <&> (\x -> (guardId x, totalSleep x))
-  & M.fromListWith (+)
-
 processShift :: [LogEntry] -> Shift
-processShift ((LogEntry (Begin guard) startTime):events) = do
-  let sleepTime = processSleep events
+processShift ((LogEntry (Begin guard) startTime):events) =
+  let sleepTime = eventsToSleepRanges events in
   Shift { guardId = guard
         , start = startTime
         , events = events
         , sleepPeriods = sleepTime
-        , totalSleep = sum sleepTime }
+        , totalSleep = length sleepTime }
 processShift _ = error "first event isn't a shift start, bad grouping"
 
-processSleep :: [LogEntry] -> [Int]
-processSleep [] = []
+sumSleepByGuard :: [Shift] -> Map Guard Int
+sumSleepByGuard xs = xs
+  <&> (\x -> (guardId x, totalSleep x))
+  & M.fromListWith (+)
 
-processSleep ((LogEntry FallAsleep t1) : (LogEntry WakeUp t2) : rest) = 
-  (diffTimesInMin t1 t2) : processSleep rest
-
-processSleep _ = error "Bad events, sleep not followed by wake"
+eventsToSleepRanges :: [LogEntry] -> [Int]
+eventsToSleepRanges [] = []
+eventsToSleepRanges ((LogEntry FallAsleep t1) : (LogEntry WakeUp t2) : rest) = 
+  (todPairToRange t1 t2) <> eventsToSleepRanges rest
+eventsToSleepRanges _ = error "Bad events, sleep not followed by wake"
 
 -- hideous date manipulation stripped from time-1.9.2,
 -- because stackage has old versions even on nightly,
 -- and I don't feel like fighting stack
 
-addLocalTime :: NominalDiffTime -> LocalTime -> LocalTime
-addLocalTime x = utcToLocalTime utc . addUTCTime x . localTimeToUTC utc
-
-diffLocalTime :: LocalTime -> LocalTime -> NominalDiffTime
-diffLocalTime a b = diffUTCTime (localTimeToUTC utc a) (localTimeToUTC utc b)
-
-diffTimesInMin :: LocalTime -> LocalTime -> Int
-diffTimesInMin t1 t2 = do
-  let diff = diffLocalTime t2 t1
-  let (LocalTime c t) = addLocalTime diff (LocalTime (fromGregorian 0 1 1) midnight) -- can't create 0000/0/0 AD
-  let (_, _, daysDiff) = toGregorian c
-  let (TimeOfDay hoursDiff minutesDiff _) = t
-  let daysDiffInMin  = 24 * 60 * (daysDiff - 1)
-  let hourDiffInMin = 60 * hoursDiff
-  daysDiffInMin + hourDiffInMin + minutesDiff
-
-todToMinutes :: LocalTime -> Int
-todToMinutes (LocalTime _ (TimeOfDay h m _)) = (h * 60) + m
+todToMinutes :: TimeOfDay -> Int
+todToMinutes (TimeOfDay h m _) = (h * 60) + m
 
 todPairToRange :: LocalTime -> LocalTime -> [Int]
-todPairToRange a b = [todToMinutes a .. todToMinutes b - 1]
+todPairToRange (LocalTime d1 t1) (LocalTime d2 t2) = case diffDays d1 d2 of
+  0 -> [todToMinutes t1 .. todToMinutes t2 - 1]
+  1 -> [todToMinutes t1 .. beforeMidnight ] <> [todToMinutes midnight .. todToMinutes t2]
+  _ -> error "two day shift, what?"
+  where beforeMidnight = todToMinutes (TimeOfDay 23 59 00)
 
 -- boring parsing stuff
 
